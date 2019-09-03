11936667e7d3733ac22d09b462d371e250122eba
Yura Istomin <yura.ist@icloud.com>
2019-09-03 14:40:52 +0300

    Initial Commit

---
+++ ConvenientLayout/ConvenientLayout.h
@@ -0,0 +1,19 @@
+//
+//  ConvenientLayout.h
+//  ConvenientLayout
+//
+//  Created by Yura Istomin on 03/09/2019.
+//  Copyright © 2019 Yura Istomin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConvenientLayout.
+FOUNDATION_EXPORT double ConvenientLayoutVersionNumber;
+
+//! Project version string for ConvenientLayout.
+FOUNDATION_EXPORT const unsigned char ConvenientLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConvenientLayout/PublicHeader.h>
+
+
